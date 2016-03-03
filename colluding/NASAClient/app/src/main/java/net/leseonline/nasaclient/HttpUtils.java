f59399f6029c4e137c89d5a38e00bd35236af0bd
mvlese <lesemv@gmail.com>
2016-03-02 22:51:23 -0500

    Called web service getting ready to upload contacts.

---
+++ colluding/NASAClient/app/src/main/java/net/leseonline/nasaclient/HttpUtils.java
@@ -0,0 +1,35 @@
+package net.leseonline.nasaclient;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+/**
+ * Created by mlese on 3/2/2016.
+ */
+public class HttpUtils {
+    private static final String BASE_URL = "http://jot.leseonline.net/capi.php";
+
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+
+    public static void getByUrl(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(url, params, responseHandler);
+    }
+
+    public static void postByUrl(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(url, params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+}
--- colluding/NASAClient/app/src/main/java/net/leseonline/nasaclient/RemoteService.java
+++ colluding/NASAClient/app/src/main/java/net/leseonline/nasaclient/RemoteService.java
@@ -2,6 +2,8 @@ package net.leseonline.nasaclient;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
@@ -9,11 +11,27 @@ import android.os.Messenger;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.loopj.android.http.*;
+import org.json.*;
+
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
+
 /**
  * This service provides an external port for remote access by a known application.
  */
 public class RemoteService extends Service {
     private final String TAG = "RemoteService";
+
+    private final int IDLE = 0;
+    private final int LOGON = 1;
+    private final int GET_ENTITY = 2;
+
+    private int state = IDLE;
+    private String token = "";
 
     private Messenger mMessenger = new Messenger(new MyHandler());
 
@@ -32,23 +50,112 @@ public class RemoteService extends Service {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
+            String contactsAsJson = "not set";
+            Bundle bundle = msg.getData();
+            if (bundle != null) {
+                contactsAsJson = bundle.getString("contacts");
+            }
             switch (msg.what) {
                 case SAY_HI: {
-                    Toast.makeText(getApplicationContext(), "HI, BBSTAT", Toast.LENGTH_LONG).show();
-                    doWork();
+                    Toast.makeText(getApplicationContext(), "HI, BBSTAT", Toast.LENGTH_SHORT).show();
+                    doWork(contactsAsJson);
                     break;
                 }
                 case SAY_HELLO:
-                    Toast.makeText(getApplicationContext(), "HELLO, BBSTAT", Toast.LENGTH_LONG).show();
-                    doWork();
+                    Toast.makeText(getApplicationContext(), "HELLO, BBSTAT", Toast.LENGTH_SHORT).show();
+                    doWork(contactsAsJson);
                     break;
             }
         }
 
-        private void doWork() {
+        private void getEntity(JsonHttpResponseHandler handler) {
+            try {
+                if (!token.isEmpty()) {
+                    JSONObject req = new JSONObject();
+                    req.put("method", "getEntity");
+                    req.put("args", new JSONObject()
+                            .put("token", token)
+                            .put("key", "Hoss Cartwright"));
+                    String jsonString = req.toString();
+                    Log.d(TAG, jsonString);
+                    String jsonStringEnc = URLEncoder.encode(jsonString, "US-ASCII");
+                    Log.d(TAG, jsonStringEnc);
+
+                    RequestParams rp = new RequestParams();
+                    rp.add("api", jsonStringEnc);
+                    Log.d(TAG, rp.toString());
+
+                    state = GET_ENTITY;
+                    HttpUtils.post("", rp, handler);
+                }
+            } catch (Exception ex){
+                Log.d(TAG, "Failed to getEntity.");
+                ex.printStackTrace();
+            }
+        }
+
+        private void doWork(String contactsAsJson) {
             try {
                 Log.d(TAG, "Received contact from remote service.");
-            } catch(Exception ex) {
+                Log.d(TAG, contactsAsJson);
+
+                JsonHttpResponseHandler handler = new JsonHttpResponseHandler() {
+
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        // If the response is JSONObject instead of expected JSONArray
+                        Log.d(TAG, "---------------- this is response : " + response);
+                        try {
+                            JSONObject serverResp = new JSONObject(response.toString());
+                            String errorMessage = serverResp.getString("errorMessage");
+                            if (errorMessage.isEmpty()) {
+                                switch(state){
+                                    case LOGON:
+                                        token = serverResp.getString("retval");
+                                        Log.d(TAG, "---------------- token : " + token);
+                                        getEntity(this);
+                                        break;
+                                    case GET_ENTITY:
+                                        Log.d(TAG, "---------------- getEntity : ");
+                                        break;
+                                }
+                            }
+                        } catch (JSONException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
+                        // Pull out the first event on the public timeline
+                        Log.d(TAG, "---------------- in onSuccess");
+                    }
+                };
+
+                JSONObject req = new JSONObject();
+                req.put("method", "logon");
+                req.put("args", new JSONObject()
+//                        .put("username", "cs590")
+//                        .put("password", "baseb@l!"));
+                        .put("username", "mlese")
+                        .put("password", "P@ssw0rd"));
+                //String jsonString = "{\"method\":\"logon\",\"args\":{\"username\":\"cs590\",\"password\":\"baseb@l!\"}}";
+//                String jsonStringEnc = "api=" + URLEncoder.encode(jsonString, "US-ASCII");
+                String jsonString = req.toString();
+                Log.d(TAG, jsonString);
+                String jsonStringEnc = URLEncoder.encode(jsonString, "US-ASCII");
+                Log.d(TAG, jsonStringEnc);
+
+                RequestParams rp = new RequestParams();
+                rp.add("api", jsonStringEnc);
+                Log.d(TAG, rp.toString());
+
+                token = "";
+                state = LOGON;
+                HttpUtils.post("", rp, handler);
+
+            } catch (Exception ex) {
                 Log.d(TAG, "Failed to send contacts by remote invocation.");
                 ex.printStackTrace();
             }
